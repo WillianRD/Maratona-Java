1fca7cb54cac1650da223e64f98d160ea45c4e90
WillRD <willianrodriguesdos1@gmail.com>
2024-05-04 01:17:06 -0300

    Maratona Java

---
+++ src/academy/devdojo/Aula01DevDojo.java
@@ -0,0 +1,15 @@
+package academy.devdojo;
+/**
+ * Isso é um comentário <br>javadoc </br> */
+public class Aula01DevDojo {
+    //Isso é um comentário de uma linha
+     /*
+     isso e um comentário
+     de múltiplas
+     linhas
+      */
+    public static void main(String [] args){
+        System.out.println("Olá Dev");
+
+    }
+}
