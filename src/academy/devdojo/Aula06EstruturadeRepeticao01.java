2914aaccf9fb84b8cebd2dbfd4696db3da5faadf
WillRD <willianrodriguesdos1@gmail.com>
2024-05-05 23:27:59 -0300

    Apredendo while, do-while & for

--- src/academy/devdojo/Aula06EstruturadeRepeticao01.java
+++ src/academy/devdojo/Aula06EstruturadeRepeticao01.java
@@ -1,2 +1,20 @@
-package academy.devdojo;public class Aula06EstruturadeRepeticao01 {
+package academy.devdojo.maratonajava;
+
+public class Aula06EstruturadeRepeticao01 {
+    public static void main(String[] args) {
+        // while, do-while, for
+        int count = 0;
+        while (count < 10) {
+            System.out.println(++count);
+        }
+        count = 0;
+        do {
+            System.out.println("dentro do do-while " + count);
+            count++;
+        } while (count < 10);
+
+        for(int i=0;i<10;i++){
+            System.out.println("For "+i);
+        }
+    }
 }
