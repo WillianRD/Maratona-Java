b209880a00dc7ea1e66cb5af0bfc7142bc45dd53
WillRD <willianrodriguesdos1@gmail.com>
2024-05-13 21:01:49 -0300

    Array

---
+++ src/academy/devdojo/Aula08ArraaysMultidimensionais01.java
@@ -0,0 +1,11 @@
+package academy.devdojo;
+
+public class Aula07ArraaysMultidimensionais01 {
+    public static void main(String[] args) {
+        // 1,2,3,4,5 Meses
+        // 31, 28, 31, 30 Dias
+        int[][] dias = new int[2][];
+        dias =
+    }
+
+}
