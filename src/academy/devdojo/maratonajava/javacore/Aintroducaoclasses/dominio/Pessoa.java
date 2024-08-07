3dd9a8e19183f9c4c881fca838686f6b474dfeb3
WillRD <willianrodriguesdos1@gmail.com>
2024-08-06 22:25:05 -0300

    Getter & Setters

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Pessoa.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Pessoa.java
@@ -3,36 +3,103 @@ package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
 public class Pessoa {
     private String nome;
     private int idade;
+    public char sexo;
+    private int cpf;
+    private int altura;
+//    private int cabelo;
+//    private String corDaPele;
+//    private String olhos;
 
-
-    public void Imprime(){
+    public void Imprime() {
         System.out.println(this.nome);
         System.out.println(this.idade);
+        System.out.println(this.cpf);
+        System.out.println(this.altura);
+        System.out.println(this.sexo);
     }
 
-    public void setNome(String nome){
-        if(nome.length() <= 3){
-            System.out.println("Seu usuário tem que ter no minimo três caracteres ");
+    public void setNome(String nome) {
+        if (nome.length() < 3) {
+            System.out.println("NOME: Seu usuário tem que ter no minimo três caracteres ");
             return;
         }
         this.nome = nome;
     }
-// Setar uma idade + validação do método
-    public void setIdade(int idade){
-        if(idade <= 0){
-            System.out.println("Idade Inválida");
+
+    public void setIdade(int idade) {
+        if (idade <= 0) {
+            System.out.println("IDADE: Idade Inválida");
             return;
         }
         this.idade = idade;
     }
-//    Retornando o valor do nome
-    public String getNome(){
+
+    public void setCpf(int cpf) {
+        if (cpf <= 11) {
+            System.out.println("Seu CPF não pode estar vazio ");
+        }
+        this.cpf = 0;
+    }
+
+    public void setAltura(int altura) {
+        if (altura == 0) {
+            System.out.println("Altura não informada");
+        }
+        this.altura = 120;
+    }
+
+    public void setSexo(char sexo) {
+        switch (sexo) {
+            case 'M':
+                System.out.println("Masculino");
+                break;
+            case 'F':
+                System.out.println("Feminino");
+                break;
+
+            default:
+                System.out.println("Sexo invalido");
+        }
+        this.sexo = sexo;
+    }
+
+    public void setCabelo(byte cabelo) {
+        switch (cabelo) {
+            case 1:
+                System.out.println("Morena");
+                break;
+            case 2:
+                System.out.println("Loira");
+                break;
+            case 3:
+                System.out.println("Ruiva");
+                break;
+            default:
+                System.out.println("Nenhum definido");
+        }
+    }
+
+    public String getNome() {
         return this.nome;
     }
 
-    //    Retornando o valor da idade
-    public int getIdade(){
+    public int getIdade() {
         return this.idade;
+    }
 
+    public char getSexo() {
+        return this.sexo;
+    }
+
+    public int getCpf() {
+        return this.cpf;
+    }
+
+    public int getAltura() {
+        return this.altura;
+    }
+
+    public char sexo(char m) {
+        return this.sexo;
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/PessoaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/PessoaTest01.java
@@ -6,10 +6,16 @@ public class PessoaTest01 {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
 
-        pessoa.setNome("Will");
+        pessoa.setNome("Willian");
         pessoa.setIdade(20);
-//        pessoa.Imprime();
-        System.out.println(pessoa.getNome());
-        System.out.println(pessoa.getIdade());
+        pessoa.setCpf(401);
+        pessoa.setAltura(120);
+        pessoa.sexo('M');
+        //  pessoa.Imprime();
+        System.out.println("Seu nome: " +pessoa.getNome());
+        System.out.println("Sua idade: " +pessoa.getIdade());
+        System.out.println("Seu CPF: " +pessoa.getCpf());
+        System.out.println("Sua Altura: " +pessoa.getAltura());
+        System.out.println("Seu sexo é: "+pessoa.getSexo());
     }
 }
