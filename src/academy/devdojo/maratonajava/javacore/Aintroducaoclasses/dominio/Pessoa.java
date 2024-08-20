3c426be869e79c08f7e23ee2008548d029a966ab
WillRD <willianrodriguesdos1@gmail.com>
2024-08-20 18:20:33 -0300

    Atualização + Implementação de novos atributos da classe Pessoa

--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Pessoa.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/dominio/Pessoa.java
@@ -2,19 +2,16 @@ package academy.devdojo.maratonajava.javacore.Aintroducaoclasses.dominio;
 
 public class Pessoa {
     // Variaveis que são usadas para guardar informações da classe Pessoa
-    private String nome;
-    private int idade;
-    private String sexo;
-    private String cpf;
+    private String nome, sexo, cpf, cabelo, suaCorFavorita, ImovelPessoal, temCarroOuMoto, corDosOlgos;
+    private int idade, verificarQuantidadeDeVeiculos;
     private double altura;
-    private String cabelo;
 
     //    Método setter que busca o nome da pessoa e faz uma validação caso se for menor
 //    que três caracteres, ele imprime uma menssagem de erro
     public void setNome(String nome) {
         if (nome.length() >= 3) {
             this.nome = nome;
-        }else {
+        } else {
             System.out.println("ERRO: Formato do nome e menor que três");
         }
     }
@@ -22,8 +19,8 @@ public class Pessoa {
     //    Método para verificar a idade, se for < || = o print mostra o erro
     public void setIdade(int idade) {
         if (idade > 0) {
-        this.idade = idade;
-        }else{
+            this.idade = idade;
+        } else {
             System.out.println("Idade Inválida");
         }
     }
@@ -32,15 +29,16 @@ public class Pessoa {
     public void setCpf(String cpf) {
         if (cpf.length() >= 11) {
             this.cpf = cpf;
-        }else {
+        } else {
             System.out.println("CPF Inválido");
         }
     }
 
     // Caso a altura nãao seja informada, mostra um print na tela de Erro
     public void setAltura(double altura) {
-        if (altura == 0) {
+        if (altura > 1) {
             this.altura = altura;
+        } else {
             System.out.println("ERRO: Altura não informada");
         }
     }
@@ -63,13 +61,93 @@ public class Pessoa {
         switch (cabelo) {
             case "Morena":
                 this.cabelo = cabelo;
+                break;
             case "Loira":
                 this.cabelo = cabelo;
+                break;
             case "Ruiva":
                 this.cabelo = cabelo;
                 break;
             default:
                 System.out.println("ERRO: Nenhum tipo de cabelo definido");
+        }
+    }
+
+    public void setSuaCorFavorita(String suaCorFavorita) {
+        String[] CorFavorita = new String[11];
+        CorFavorita[0] = "Vermelho";
+        CorFavorita[1] = "Azul";
+        CorFavorita[2] = "Preto";
+        CorFavorita[3] = "Marrom";
+        CorFavorita[4] = "Laranja";
+        CorFavorita[5] = "Amarelo";
+        CorFavorita[6] = "Verde";
+        CorFavorita[7] = "Cinza";
+        CorFavorita[8] = "Branco";
+        CorFavorita[9] = "Rosa";
+        CorFavorita[10] = "Roxo";
+        for (String coresFavoritas : CorFavorita) {
+            if (coresFavoritas.equals(suaCorFavorita)) {
+                this.suaCorFavorita = suaCorFavorita;
+                break;
+            }
+        }
+}
+
+    public void setCorDosOlgos(String corDosOlgos) {
+        String[] CorDosOlhos = new String[8];
+        CorDosOlhos[0] = "Castanhos Escuros";
+        CorDosOlhos[1] = "Castanhos Claro";
+        CorDosOlhos[2] = "Verde Claro";
+        CorDosOlhos[3] = "Verde Escuro";
+        CorDosOlhos[4] = "Azul Claro";
+        CorDosOlhos[5] = "Azul Escuro";
+        CorDosOlhos[6] = "Avalã";
+        CorDosOlhos[7] = "Preto";
+        for (String coresOlhos : CorDosOlhos) {
+            if (coresOlhos.equals(corDosOlgos)) {
+                this.corDosOlgos = corDosOlgos;
+                break;
+            }
+            if (!CorDosOlhos.equals(corDosOlgos)) {
+                System.out.println("COR DOS OLHOS: Erro ao adicionar a cor dos olhos");
+                break;
+            }
+        }
+    }
+
+    public void setImovelPessoal(String ImovelPessoal) {
+        switch (ImovelPessoal) {
+            case "Sim":
+                this.ImovelPessoal = ImovelPessoal;
+                break;
+            case "Não":
+                this.ImovelPessoal = ImovelPessoal;
+                break;
+            default:
+                System.out.println("Não reconheci a escolha entre 'Sim' & 'Não' ");
+        }
+    }
+
+    public void setTemCarroOuMoto(String temCarroOuMoto) {
+        switch (temCarroOuMoto) {
+            case "Carro":
+                this.temCarroOuMoto = temCarroOuMoto;
+            case "Moto":
+                this.temCarroOuMoto = temCarroOuMoto;
+            case "Não Tenho":
+                this.temCarroOuMoto = temCarroOuMoto;
+                break;
+            default:
+                System.out.println("Aguardando uma resposta ");
+        }
+    }
+
+    public void setVerificarQuantidadeDeVeiculos(int verificarQuantidadeDeVeiculos) {
+        if (verificarQuantidadeDeVeiculos > 0) {
+            this.verificarQuantidadeDeVeiculos = verificarQuantidadeDeVeiculos;
+        } else {
+            System.out.println(" Quantidade: O valor não pode ser < ou = 0");
         }
     }
 
@@ -96,5 +174,24 @@ public class Pessoa {
     public String getCabelo() {
         return this.cabelo;
     }
-}
 
+    public String getSuaCorFavorita() {
+        return this.suaCorFavorita;
+    }
+
+    public String getCorDosOlgos() {
+        return this.corDosOlgos;
+    }
+
+    public String getImovelPessoal() {
+        return this.ImovelPessoal;
+    }
+
+    public String getTemCarroOuMoto() {
+        return this.temCarroOuMoto;
+    }
+
+    public int getVerificarQuantidadeDeVeiculos() {
+        return this.verificarQuantidadeDeVeiculos;
+    }
+}
--- src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/PessoaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Aintroducaoclasses/test/PessoaTest01.java
@@ -6,21 +6,23 @@ public class PessoaTest01 {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
 
-        System.out.println("LISTA DE ERROS");
-
-        pessoa.setNome("ana");
-        pessoa.setIdade(1);
+        pessoa.setNome("Ana");
+        pessoa.setIdade(20);
         pessoa.setCpf("123456789-00");
         pessoa.setAltura(1.83);
-        pessoa.setSexo("Masculino");
+        pessoa.setSexo("Feminino");
         pessoa.setCabelo("Ruiva");
+        pessoa.setSuaCorFavorita("Azul");
+        pessoa.setCorDosOlgos("Castanhos Escuros");
+        pessoa.setImovelPessoal("Sim");
+        pessoa.setTemCarroOuMoto("Carro");
+        pessoa.setVerificarQuantidadeDeVeiculos(5);
 
-        System.out.println("---------------------------");
-        System.out.println("Seu nome: "+pessoa.getNome());
-        System.out.println("Sua idade: "+pessoa.getIdade());
-        System.out.println("Seu CPF: "+pessoa.getCpf());
-        System.out.println("Sua Altura é: "+pessoa.getAltura());
-        System.out.println("Seu sexo: " +pessoa.getSexo());
-        System.out.println("Seu tipo de cabelo é: "+pessoa.getCabelo());
+        System.out.println("--------------------------------");
+        System.out.println("Registro de Pessoa");
+        System.out.println("Seu nome: "+pessoa.getNome() + "\n" +"Sua idade: "+pessoa.getIdade() + "\n" + "Seu CPF: "+pessoa.getCpf());
+        System.out.println("Sua Altura é: "+pessoa.getAltura() + "\n" + "Seu sexo: " +pessoa.getSexo() + "\n" + "Seu tipo de cabelo é: "+pessoa.getCabelo());
+        System.out.println("Sua cor Favorita é: "+pessoa.getSuaCorFavorita() + "\n" + "Sua cor dos olhos é: "+pessoa.getCorDosOlgos());
+        System.out.println("Você tem imovel:" + pessoa.getImovelPessoal() + "\n" + "Você tem Carro ou Moto: " +pessoa.getTemCarroOuMoto() + "\n" + "Quantidade De Veículos: "+pessoa.getVerificarQuantidadeDeVeiculos());
     }
 }
