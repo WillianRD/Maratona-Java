19e40c0a8b73b37d878986b540a1e421dad17f47
WillianRD <willianrodriguesdos1@gmail.com>
2024-10-06 19:16:19 -0300

    Padrões de Projeto Singleton Eager Initicializion

---
+++ src/academy/devdojo/javacore/ZZHpadroesdeprojeto/dominio/AircraftSingletonEager.java
@@ -0,0 +1,26 @@
+package academy.devdojo.javacore.ZZHpadroesdeprojeto.dominio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AircraftSingletonEager {
+    private static final AircraftSingletonEager INSTANCE = new AircraftSingletonEager("787-900");
+    private final Set<String> avaliableSeats = new HashSet<>();
+    private final String name;
+
+    public AircraftSingletonEager(String name){
+        this.name = name;
+    }
+
+    {
+        avaliableSeats.add("1A");
+        avaliableSeats.add("1B");
+    }
+
+    public static AircraftSingletonEager getInstance(){
+        return INSTANCE;
+    }
+        public boolean bookSeat(String seat){
+            return avaliableSeats.remove(seat);
+        }
+}
---
+++ src/academy/devdojo/javacore/ZZHpadroesdeprojeto/test/AircraftSingletonTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.javacore.ZZHpadroesdeprojeto.dominio.AirCraft;
+import academy.devdojo.javacore.ZZHpadroesdeprojeto.dominio.AircraftSingletonEager;
+
+public class AircraftSingletonTest01 {
+    public static void main(String[] args) {
+        bookSeat("1A");
+        bookSeat("1A");
+        AircraftTest01.bookSeat("1A");
+    }
+
+    private static void bookSeat(String seat){
+        System.out.println(AircraftSingletonEager.getInstance());
+        AircraftSingletonEager aircraftSingletonEager = AircraftSingletonEager.getInstance();
+        System.out.println(aircraftSingletonEager.bookSeat(seat));
+    }
+}
