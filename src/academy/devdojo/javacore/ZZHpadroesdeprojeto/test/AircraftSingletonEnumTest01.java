8581e711428d4b20c1903ce4f8b06347c88ec1f7
WillianRD <willianrodriguesdos1@gmail.com>
2024-10-06 20:18:54 -0300

    Padrões de Projeto Singleton Enumarate

---
+++ src/academy/devdojo/javacore/ZZHpadroesdeprojeto/test/AircraftSingletonEnumTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.javacore.ZZHpadroesdeprojeto.dominio.AircraftSingletoEnum;
+
+public class AircraftSingletonEnumTest01 {
+    public static void main(String[] args) {
+        compraAssentos("1A");
+        compraAssentos("1A");
+    }
+
+    private static void compraAssentos(String assentos) {
+        System.out.println(AircraftSingletoEnum.INSTANCIA.hashCode()); //Check
+        AircraftSingletoEnum instancia = AircraftSingletoEnum.INSTANCIA;
+        System.out.println(instancia.comprandoAssentos(assentos));
+    }
+}
