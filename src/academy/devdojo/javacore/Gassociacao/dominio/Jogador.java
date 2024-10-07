ff6bdb34e5838b574216f657e1dcd65a1d44efbe
WillianRD <willianrodriguesdos1@gmail.com>
2024-10-06 22:05:35 -0300

    Associação  Associação unidirecional um para muitos

--- src/academy/devdojo/javacore/Gassociacao/dominio/Jogador.java
+++ src/academy/devdojo/javacore/Gassociacao/dominio/Jogador.java
@@ -2,21 +2,31 @@ package academy.devdojo.javacore.Gassociacao.dominio;
 
 public class Jogador {
     private String nome;
+    private Time time;
 
     public Jogador(String nome) {
         this.nome = nome;
     }
 
     public void imprime() {
-        System.out.println("Jogador Brasileiro: " + this.nome);
-        System.out.println("Teste");
+        System.out.println("Jogador Brasileiro (a): " + this.nome);
+        if (time != null) {
+            System.out.println("Time: " + time.getNome());
+        }
     }
-
     public String getNome() {
         return this.nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public void setTime (Time time){
+        this.time = time;
+    }
+
+    public Time getTime(){
+        return this.time;
     }
 }
