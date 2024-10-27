0c6d12a03d8249e1688c851f12fa1f190d40a02f
WillianRD <willianrodriguesdos1@gmail.com>
2024-10-27 14:56:46 -0300

    Enum - Parte 3 Sobrescrita de métodos

---
+++ src/main/javacore/Kenum/dominio/TipoDePagamento.java
@@ -0,0 +1,17 @@
+package main.javacore.Kenum.dominio;
+
+public enum TipoDePagamento {
+    DEBITO {
+        @Override
+        public double calcularDesconto(double valor) {
+            return valor * 0.1;
+        }
+
+    }, CREDITO {
+        public double calcularDesconto(double valor) {
+            return valor * 0.05;
+        }
+    };
+
+    public abstract double calcularDesconto(double valor);
+}
--- src/main/javacore/Kenum/test/ClientesTest01.java
+++ src/main/javacore/Kenum/test/ClientesTest01.java
@@ -2,17 +2,15 @@ package main.javacore.Kenum.test;
 
 import main.javacore.Kenum.dominio.Clientes;
 import main.javacore.Kenum.dominio.TipoCliente;
+import main.javacore.Kenum.dominio.TipoDePagamento;
 
 public class ClientesTest01 {
     public static void main(String[] args){
-        Clientes cliente = new Clientes("Will", TipoCliente.PESSOA_FISICA);
-        Clientes cliente2 = new Clientes("Yuri",TipoCliente.PESSOA_JURIDICA);
-        Clientes cliente3 = new Clientes("Leide", TipoCliente.PESSOA_JURIDICA);
-        Clientes cliente4 = new Clientes("Wiu",TipoCliente.PESSOA_FISICA);
+        Clientes cliente1 = new Clientes("WIllian", TipoCliente.PESSOA_FISICA, TipoDePagamento.DEBITO);
+        Clientes clientes2 = new Clientes("Leide", TipoCliente.PESSOA_JURIDICA, TipoDePagamento.CREDITO);
 
-        System.out.println(cliente);
-        System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
+        System.out.println(cliente1);
+        System.out.println(clientes2);
+        System.out.println(TipoDePagamento.DEBITO.calcularDesconto(100));
     }
 }
