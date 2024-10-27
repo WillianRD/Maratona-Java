0ccde9a72a22698e73d54bef072822156bc21f6f
WillianRD <willianrodriguesdos1@gmail.com>
2024-10-27 14:58:33 -0300

    Enum - Parte 3 Sobrescrita de métodos

--- src/main/javacore/Kenum/dominio/TipoCliente.java
+++ src/main/javacore/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,24 @@
 package main.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, " Pessoa Fisica"),
+    PESSOA_JURIDICA(2, " Pessoa Juridica");
+
+    public int valor;
+    public String nomeRelatorio;
+
+    //    Contrutor da classe Enum
+    TipoCliente(int valor, String nomeRelatorio) {
+        this.valor = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public int getValor(){
+        return valor;
+    }
+
+    public String getNomeRelatorio(){
+        return nomeRelatorio;
+    }
+
 }
