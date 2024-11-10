0e02fc4d9eb19ea10763e25616f80699ce405960
WillianRD <willianrodriguesdos1@gmail.com>
2024-11-10 05:46:01 -0300

    FIleWriter0

---
+++ src/main/java/academy/devdojo/javacore/Vio/test/FIleWriter01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.javacore.Vio.test;
+// File
+// FileWriter
+// FileReader
+// BufferWriter
+// BufferReader
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FIleWriter01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+
+        try( FileWriter fileWriter = new FileWriter(file,true)) {
+            fileWriter.write(" Somente um arquivo para teste \n Continuando a melhor Saga de Linguagem de programação ");
+            fileWriter.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
