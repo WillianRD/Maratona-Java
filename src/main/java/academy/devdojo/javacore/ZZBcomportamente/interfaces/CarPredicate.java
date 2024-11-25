9d5a85fc13bd02b0890ff4ee642a776be262c4c6
WillianRD <willianrodriguesdos1@gmail.com>
2024-11-25 05:45:59 -0300

    FunctionalInterface

--- src/main/java/academy/devdojo/javacore/ZZBcomportamente/interfaces/CarPredicate.java
+++ src/main/java/academy/devdojo/javacore/ZZBcomportamente/interfaces/CarPredicate.java
@@ -2,6 +2,7 @@ package academy.devdojo.javacore.ZZBcomportamente.interfaces;
 
 import academy.devdojo.javacore.ZZBcomportamente.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
-    boolean test(Car car);
+    boolean test (Car car);
 }
