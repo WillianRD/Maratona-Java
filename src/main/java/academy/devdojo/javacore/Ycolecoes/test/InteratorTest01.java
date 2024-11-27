8c9801d7ce54d6726179778b9011f68d6b0d0f12
WillianRD <willianrodriguesdos1@gmail.com>
2024-11-27 05:51:55 -0300

    Interator with List LinkedList

--- src/main/java/academy/devdojo/javacore/Ycolecoes/test/InteratorTest01.java
+++ src/main/java/academy/devdojo/javacore/Ycolecoes/test/InteratorTest01.java
@@ -2,28 +2,34 @@ package academy.devdojo.javacore.Ycolecoes.test;
 
 import academy.devdojo.javacore.Ycolecoes.dominio.Manga;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InteratorTest01 {
     public static void main(String[] args) {
-        List<Manga> mangas = new ArrayList<>(6);
-        mangas.add(new Manga(1L, "Attack on titan", 19.9D,0));
-        mangas.add(new Manga(1L, "Bersek", 19.9D,0));
-        mangas.add(new Manga(2L, "Berserk", 12.55D,5));
-        mangas.add(new Manga(3L, "Pokemon", 15.6D,2));
-        mangas.add(new Manga(5L, "Dragon Ball Z", 10.5D,1));
+        List<Manga> mangas = new LinkedList<>();
+        mangas.add(new Manga(1L, "Attack on titan", 19.9D, 0));
+        mangas.add(new Manga(1L, "Bersek", 19.9D, 0));
+        mangas.add(new Manga(2L, "Berserk", 12.55D, 0));
+        mangas.add(new Manga(3L, "Pokemon", 15.6D, 0));
+        mangas.add(new Manga(5L, "Dragon Ball Z", 10.5D, 0));
 
 //        Iterator<Manga> mangaInterator = mangas.iterator();
-//        while (mangaInterator.hasNext()){
+//        while (mangaInterator.hasNext()) {
 //            Manga manga = mangaInterator.next();
-//            if(manga.getQuantidade() == 0){
+//            if (manga.getQuantidade() == 0) {
 //                mangaInterator.remove();
-
-//        remova-se manga.getQuantidade for == 0
-//        manga é uma variavel de refêrencia
+//                    System.out.println(manga);
+//            }
+//        }
         mangas.removeIf(manga -> manga.getQuantidade() == 0);
         System.out.println(mangas);
+        if (mangas.size()<= 0) {
+            throw new RuntimeException("Mangas sem quantidade");
         }
 
     }
+}
