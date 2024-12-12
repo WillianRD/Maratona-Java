8df8afcb4480873b52f0a112e5f6d9cd2cd0938a
WillianRD <willianrodriguesdos1@gmail.com>
2024-12-11 21:24:57 -0300

    Internacionalização SimplesDateFormat..

---
+++ src/main/java/academy/devdojo/javacore/Sformatacao/test/SimplesDateFormat.java
@@ -0,0 +1,25 @@
+package academy.devdojo.javacore.Sformatacao.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimplesDateFormat {
+    public static void main(String[] args) {
+        String pattern = "'São Paulo' dd, 'de' MMM 'de' yyyy";
+        SimpleDateFormat simplesDateFormat = new SimpleDateFormat(pattern);
+        System.out.println(simplesDateFormat.format(new Date()));
+//  Saída
+//  São Paulo--quarta-feira, 11 dez. de 2024
+// Horário de brasilia 21:12:44
+
+        try {
+            System.out.println(simplesDateFormat.parse("São Paulo 11, de dez. de 2024"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // Saída
+//         Wed Dec 11 00:00:00 GMT-03:00 2024 -- Retorna o valor padrão
+
+    }
+}
