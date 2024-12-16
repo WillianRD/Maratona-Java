7665efe226fd4a7c8b2dd6d71a122a790570ed6c
WillianRD <willianrodriguesdos1@gmail.com>
2024-12-16 05:53:10 -0300

    Class ChronoUnit

--- src/main/java/academy/devdojo/javacore/Rdatas/test/ChronoUnitTest01.java
+++ src/main/java/academy/devdojo/javacore/Rdatas/test/ChronoUnitTest01.java
@@ -7,7 +7,7 @@ import java.time.temporal.ChronoUnit;
 public class ChronoUnitTest01 {
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime aniversario = LocalDateTime.of(1988, Month.AUGUST, 6, 12, 0, 0);
+        LocalDateTime aniversario = LocalDateTime.of(2003, Month.OCTOBER, 6, 12, 0, 0);
         System.out.println(ChronoUnit.DAYS.between(aniversario, now) + " dias");
         System.out.println(ChronoUnit.WEEKS.between(aniversario, now) + " semanas");
         System.out.println(ChronoUnit.MONTHS.between(aniversario, now) + " Meses");
