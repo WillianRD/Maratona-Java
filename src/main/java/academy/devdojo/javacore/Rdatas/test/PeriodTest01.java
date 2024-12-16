934c22302c2d414e1c6923e5aeb2bdccdae9dea8
WillianRD <willianrodriguesdos1@gmail.com>
2024-12-16 05:53:40 -0300

    Class PeriodTest

--- src/main/java/academy/devdojo/javacore/Rdatas/test/PeriodTest01.java
+++ src/main/java/academy/devdojo/javacore/Rdatas/test/PeriodTest01.java
@@ -1,17 +1,17 @@
-package academy.devdojo.javacore.Sformatacao.test;
+package academy.devdojo.javacore.Rdatas.test;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTest01 {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
         LocalDate localDate = LocalDate.now().plusYears(2).plusDays(7);
         Period p1 = Period.between(now,localDate);
-        Period p2 = Period.ofDays(7);
-        Period p3 = Period.ofMonths(10);
+        Period p2 = Period.ofDays(10);
         Period p4 = Period.ofWeeks(58);
+        Period p3 = Period.ofMonths(3);
         Period p5 = Period.ofYears(3);
 
         System.out.println(p1);
@@ -19,5 +19,8 @@ public class PeriodTest01 {
         System.out.println("São " + p3 + " semanas");
         System.out.println(p4);
         System.out.println(p5);
+        System.out.println(p3.getMonths());
+        System.out.println(Period.between(now,LocalDate.now().plusYears(2)).getMonths());
+        System.out.println(now.until(now.plusDays(p3.getDays()), ChronoUnit.DAYS));
     }
 }
